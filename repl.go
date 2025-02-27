a57e26d5134bdf35ef40703845fa49c7affb0090
mocos <130593875+MoXcz@users.noreply.github.com>
2025-02-27 17:02:12 -0500

    Add basic REPL functionality

--- repl.go
+++ repl.go
@@ -1,6 +1,23 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		command := cleanInput(input)[0]
+
+		fmt.Printf("Your command was: %s\n", command)
+	}
+}
 
 func cleanInput(text string) []string {
 	cleanInput := strings.Fields(strings.ToLower(text))
