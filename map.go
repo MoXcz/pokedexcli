3a15f3910e844c06241916688265f9a4478da8b4
mocos <130593875+MoXcz@users.noreply.github.com>
2025-03-01 02:05:21 -0500

    Refactor commands to use new config type

--- map.go
+++ map.go
@@ -1,87 +1,37 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
 )
 
-type LocationArea struct {
-	Next     string `json:"next"`
-	Prev     string `json:"previous"`
-	Location []struct {
-		Name string `json:"name"`
-		Url  string `json:"url"`
-	} `json:"results"`
-}
-
-func commandMap() error {
-	url := "https://pokeapi.co/api/v2/location-area"
-	currentState := getValidCommands()["map"].config
-
-	if currentState.Next != "" {
-		url = currentState.Next
-	}
-
-	res, err := http.Get(url)
+func commandMap(cfg *config) error {
+	locations, err := cfg.pokeapiClient.GetLocationAreas(cfg.nextURL)
 	if err != nil {
-		return fmt.Errorf("Error communicating with the server: %w", err)
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode > 299 {
-		return fmt.Errorf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, res.Body)
+		return err
 	}
 
-	var areas LocationArea
+	cfg.nextURL = locations.Next
+	cfg.previousURL = locations.Previous
 
-	decoder := json.NewDecoder(res.Body)
-	err = decoder.Decode(&areas)
-	if err != nil {
-		return fmt.Errorf("Error decoding JSON: %w", err)
-	}
-
-	for _, area := range areas.Location {
+	for _, area := range locations.Results {
 		fmt.Println(area.Name)
 	}
 
-	currentState.Next = areas.Next
-	currentState.Previous = areas.Prev
-
 	return nil
 }
-func commandMapb() error {
-	currentState := getValidCommands()["map"].config
-	if currentState.Previous == "" {
-		fmt.Println("You're on the first page")
-		return nil
-	}
-	url := currentState.Previous
-
-	res, err := http.Get(url)
+func commandMapb(cfg *config) error {
+	locations, err := cfg.pokeapiClient.GetLocationAreas(cfg.previousURL)
 	if err != nil {
-		return fmt.Errorf("Error communicating with the server: %w", err)
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode > 299 {
-		return fmt.Errorf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, res.Body)
+		return err
 	}
 
-	var areas LocationArea
+	cfg.nextURL = locations.Next
+	cfg.previousURL = locations.Previous
 
-	decoder := json.NewDecoder(res.Body)
-	err = decoder.Decode(&areas)
-	if err != nil {
-		return fmt.Errorf("Error decoding JSON: %w", err)
-	}
-
-	for _, area := range areas.Location {
+	for _, area := range locations.Results {
 		fmt.Println(area.Name)
 	}
 
-	currentState.Next = areas.Next
-	currentState.Previous = areas.Prev
-
 	return nil
+
 }
