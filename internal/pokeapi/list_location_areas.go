824c04cc15238b85366104955ef7cca5b5aa6e4b
mocos <130593875+MoXcz@users.noreply.github.com>
2025-03-02 15:32:37 -0500

    Refactor location areas method to use caching

--- internal/pokeapi/list_location_areas.go
+++ internal/pokeapi/list_location_areas.go
@@ -3,35 +3,48 @@ package pokeapi
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func (c *Client) GetLocationAreas(url *string) (RespLocations, error) {
-	fullURL := baseURL + "/location-area"
+	fullURL := baseURL + "/location-area?offset=0&limit=20"
 	if url != nil {
 		fullURL = *url // in case there's some previous state
 	}
 
-	req, err := http.NewRequest("GET", fullURL, nil)
-	if err != nil {
-		return RespLocations{}, err
-	}
+	body, ok := c.cache.Get(fullURL)
 
-	res, err := c.httpClient.Do(req)
-	if err != nil {
-		return RespLocations{}, fmt.Errorf("Error communicating with the server: %w", err)
-	}
-	defer res.Body.Close()
+	if !ok {
+		req, err := http.NewRequest("GET", fullURL, nil)
+		if err != nil {
+			return RespLocations{}, err
+		}
 
-	if res.StatusCode > 299 {
-		return RespLocations{}, fmt.Errorf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, res.Body)
+		res, err := c.httpClient.Do(req)
+		if err != nil {
+			return RespLocations{}, fmt.Errorf("Error communicating with the server: %w", err)
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode > 299 {
+			return RespLocations{}, fmt.Errorf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, res.Body)
+		}
+
+		body, err = io.ReadAll(res.Body)
+		if err != nil {
+			return RespLocations{}, fmt.Errorf("Error reading body: %w", err)
+		}
+
+		c.cache.Add(fullURL, body)
 	}
 
 	var locationAreas RespLocations
-	decoder := json.NewDecoder(res.Body)
-	err = decoder.Decode(&locationAreas)
+
+	err := json.Unmarshal(body, &locationAreas)
 	if err != nil {
-		return RespLocations{}, fmt.Errorf("Error decoding JSON: %w", err)
+		return RespLocations{}, fmt.Errorf("Error decoding JSON %w", err)
 	}
+
 	return locationAreas, nil
 }
