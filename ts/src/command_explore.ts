f29dd25b55b67cd7ee1fbc81b1412375ea7eafdf
MoXcz <130593875+MoXcz@users.noreply.github.com>
2026-03-31 20:57:30 -0600

    fix(ts): validate input for explore command

--- ts/src/command_explore.ts
+++ ts/src/command_explore.ts
@@ -1,10 +1,16 @@
 import { State } from "./state.js";
 
-export async function commandExplore(state: State, location: string) {
+export async function commandExplore(state: State, ...args: string[]) {
+  if (args.length != 1) {
+    console.log("Invalid argument");
+    return;
+  }
+  const location = args[0];
+
   const locationResponse = await state.pokeApi.fetchLocation(location);
   const pokemon_encounters = locationResponse.pokemon_encounters;
 
-  console.log("Exploring pastoria-city-area...");
+  console.log(`Exploring ${location}...`);
   console.log("found Pokemon:");
   pokemon_encounters.forEach((enc) => {
     console.log(`  - ${enc.pokemon.name}`);
